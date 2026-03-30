958e72aa912ff4336eed892600c3d8d252d17980
shubhamggx <shubhamghansala59@gmail.com>
2026-03-30 21:56:26 +0530

    create basic ui  login and sigup textfield with button

--- lib/login.dart
+++ lib/login.dart
@@ -6,20 +6,66 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("LOGIN / REGISTRATION"),
+        backgroundColor: Colors.lightBlue,
+      ),
+      
       body: Center(
+
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("LOgin "),
-            ElevatedButton(
-              onPressed: () {
-                print("button clicked");
-              },
-              child: Text("Login"),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  label: Text(" EMAIL"),
+                  hint: Text("Enter your email id....."),
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  label: Text("PASSWORD"),
+                  hint: Text("Enter your password"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print("BUTTON LOGIN CLICKED");
+                    },
+                    child: Text("LOGIN"),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print("BUTTON SIGNUP CLICKED");
+                    },
+                    child: Text("SIGNUP"),
+                  ),
+                ),
+              ],
+            ),
+            
+            
+
           ],
         ),
-        
       ),
     );
   }
